86907010ea15abf599b93f99c5bb0dad15279dc9
Darrel Nato <johndarrelnato@gmail.com>
2018-09-07 08:03:28 +0800

    initial commit

---
+++ hero-birds.js
@@ -0,0 +1,10 @@
+VANTA.BIRDS({
+	el: "#hero-birds-bg",
+	backgroundColor: 0x0F1617,
+	color1: 0x0F1617,
+	color2: 0x0F1617,
+	wingSpan: 10.00,
+	separation: 25.00,
+	cohesion: 1.00,
+	backgroundAlpha: 1.00
+})
