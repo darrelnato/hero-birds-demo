ffb33a2eee28786a34b91d343324764dc0f9b47e
Darrel Nato <johndarrelnato@gmail.com>
2018-09-07 17:17:36 +0800

    darker color scheme

--- hero-birds.js
+++ hero-birds.js
@@ -1,10 +1,10 @@
 VANTA.BIRDS({
 	el: "#hero-birds-bg",
-	backgroundColor: 0x4F50FF,
-	color1: 0xffffff,
-	color2: 0x4330B3,
+	backgroundColor: 0xffffff,
+	color1: 0xB5B5B5,
+	color2: 0x372475,
 	wingSpan: 10.00,
-	separation: 50.00,
+	separation: 25.00,
 	cohesion: 1.00,
 	backgroundAlpha: 0.00
 })
